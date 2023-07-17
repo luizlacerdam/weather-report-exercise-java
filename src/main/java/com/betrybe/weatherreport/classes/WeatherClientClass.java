e409fb9800dc5a478ec4422de201f4806bd0e72a
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-17 14:37:56 -0300

    req-01

---
+++ src/main/java/com/betrybe/weatherreport/classes/WeatherClientClass.java
@@ -0,0 +1,10 @@
+package com.betrybe.weatherreport.classes;
+
+
+public class WeatherClientClass implements com.betrybe.weatherreport.interfaces.WeatherClient {
+
+  @Override
+  public String getWeather(String city) {
+    return "tempinho bom!";
+  }
+}
---
+++ src/main/java/com/betrybe/weatherreport/classes/WeatherConfig.java
@@ -0,0 +1,13 @@
+package com.betrybe.weatherreport.classes;
+
+import com.betrybe.weatherreport.interfaces.WeatherClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WeatherConfig {
+  @Bean
+  WeatherClient weatherClient() {
+    return new WeatherClientClass();
+  }
+}
