b1304035164adddae4b51150d085213cd1ea0538
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-07-17 15:29:15 -0300

    checkstyle

--- src/main/java/com/betrybe/weatherreport/classes/WeatherClientClass.java
+++ src/main/java/com/betrybe/weatherreport/classes/WeatherClientClass.java
@@ -1,6 +1,8 @@
 package com.betrybe.weatherreport.classes;
 
-
+/**
+ * Implementation of WeatherClientClass.
+ */
 public class WeatherClientClass implements com.betrybe.weatherreport.interfaces.WeatherClient {
 
   @Override
--- src/main/java/com/betrybe/weatherreport/classes/WeatherConfig.java
+++ src/main/java/com/betrybe/weatherreport/classes/WeatherConfig.java
@@ -4,6 +4,9 @@ import com.betrybe.weatherreport.interfaces.WeatherClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Não entendi.
+ */
 @Configuration
 public class WeatherConfig {
   @Bean
